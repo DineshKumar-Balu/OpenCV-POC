cb36db72f2dc8de8db70418ca5e0933c85efcb0c
hemanthhvv <vvhemanthh@gmail.com>
2024-07-15 16:34:18 +0530

    ffmpeg added to dependencies

--- app.py
+++ app.py
@@ -105,7 +105,6 @@ def main():
 
         st.write(initial_time)
         st.write(end_time)
-        initial_time = "00:00:00"
         if initial_time and end_time:
             c1,c2 = st.columns(2)
             with c1:
